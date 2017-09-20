1ac245ef99d2c596ab25e648772e831090a4653e
Daisy <daisy@DaisydeAir.lan>
2017-09-20 22:44:16 +0800

    start

---
+++ cstart/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cstart
+//
+//  Created by Daisy on 2017/9/20.
+//  Copyright © 2017年 Daisy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
